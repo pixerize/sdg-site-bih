f5d74eccf1c82331c84358da0fb26fcab7d090ba
pixerize <40294788+pixerize@users.noreply.github.com>
2020-11-24 13:57:17 +0000

    Deploying to gh-pages from  @ 7582ca0d7bd83840bd9ccde6fa60a00d183dbad2 🚀

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,4 @@
-(function($) {
+$( document ).ready(function() {
   $("a[href*='/1-a-3']").parent().remove();
   $("a[href*='/8-9-2']").parent().remove();
   $("a[href*='/13-3-2']").parent().remove();
